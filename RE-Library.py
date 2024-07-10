7072399ac44b47b8fa6abea3d3e731e0afee70ef
Tanmay Goraksha <140690246+Tanmay123Goraksha@users.noreply.github.com>
2024-07-10 11:03:54 +0530

    Update RE-Library.py

--- RE-Library.py
+++ RE-Library.py
@@ -12,6 +12,22 @@ matches = re.findall(pattern, grades)
 print(matches) 
 
 
+for item in re.finditer("(?P<title>[\w]+)(?=\[edit\])",wiki)
+
+
+
+# (?P<title>[\w]+): This is a named capturing group (?P<title>). It captures a sequence of one or more (+) word characters ([\w]). Word characters include alphabetic characters, digits, and underscores.
+
+# [\w] matches any word character.
+# + quantifier specifies that the previous token ([\w]) must match one or more times.
+# (?P<title> defines the start of a named capturing group with the name title.
+# (?=\[edit\]): This is a positive lookahead assertion. It specifies a position in the string where what immediately follows is [edit].
+
+# \[edit\] matches the literal string [edit].
+# (?= is the start of the lookahead assertion, indicating that what follows should match [edit].
+# \] matches the literal ].
+
+
  
 
 
